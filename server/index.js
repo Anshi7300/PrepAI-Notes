2023f722fe602777a71e6d0d2e6dd22df96b29fa
Anshika <anshika701782@gmail.com>
2026-04-01 22:00:17 +0530

    Update CORS origin for production environment

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {origin:"https://prepai-notes-kza1.onrender.com/",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
